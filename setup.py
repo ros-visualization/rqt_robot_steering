060e598c30883afb7ef1e23f3c63d4afeb3607b7
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-11-14 21:15:38 -0800

    port package to ROS 2 (#7)

--- setup.py
+++ setup.py
@@ -1,11 +1,34 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['rqt_robot_steering'],
-    package_dir={'': 'src'}
+package_name = 'rqt_robot_steering'
+setup(
+    name=package_name,
+    version='0.5.9',
+    package_dir={'': 'src'},
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name + '/resource', ['resource/RobotSteering.ui']),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['plugin.xml']),
+        ('lib/' + package_name, ['scripts/rqt_robot_steering'])
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Dirk Thomas',
+    maintainer='Dirk Thomas',
+    maintainer_email='dthomas@osrfoundation.org',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description=(
+        'rqt_robot_steering provides a GUI plugin for steering a robot using Twist messages.'
+    ),
+    license='BSD',
+    scripts=['scripts/rqt_robot_steering'],
 )
-
-setup(**d)

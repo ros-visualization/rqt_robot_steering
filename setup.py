a4cb592d0b9d6b632662b37710b3cb8a07fc5d46
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-06-01 15:23:29 +0200

    4.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 package_name = 'rqt_robot_steering'
 setup(
     name=package_name,
-    version='4.0.2',
+    version='4.1.0',
     package_dir={'': 'src'},
     packages=[package_name],
     data_files=[
